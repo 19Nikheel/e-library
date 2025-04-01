b47ba7a1827683d68237cdae87a0c69af66d940c
19Nikheel <159456710+19Nikheel@users.noreply.github.com>
2025-04-01 16:37:48 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/library/config/SecurityConfig.java
+++ src/main/java/com/example/library/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500", "http://localhost:5500")); // Allow frontend
+        configuration.setAllowedOrigins(Arrays.asList("https://e-library-frontend-kvub.onrender.com")); // Allow frontend
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
